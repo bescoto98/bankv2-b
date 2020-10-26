36dc80f29a2ef17482354a6180ca386eee7dbf44
bescoto98 <escoto.brenda42@gmail.com>
2020-10-26 09:08:51 -0700

    initial commit

---
+++ src/main/java/com/revature/Bankv2Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankv2Application.class, args);
+	}
+
+}
